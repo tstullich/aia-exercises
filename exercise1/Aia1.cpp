f94494c68b08489ae3c14943123f4dab0cf94d07
René Marius Saitenmacher <r.saitenmacher@campus.tu-berlin.de>
2017-05-07 15:53:25 +0200

    Create interesting distort/bw/glare transformation

--- exercise1/Aia1.cpp
+++ exercise1/Aia1.cpp
@@ -6,6 +6,9 @@
 // Description : 
 //============================================================================
 
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include "Aia1.h"
 
 // function that performs some kind of (simple) image processing
@@ -15,14 +18,23 @@ return	output image
 */
 Mat Aia1::doSomethingThatMyTutorIsGonnaLike(Mat& img) {
     Mat outputImg = img.clone();
-    for (int i = 0; i < img.rows; i++) {
-        for (int j = 0; j < img.cols; j++) {
-            Vec3b pixel = outputImg.at<Vec3b>(i, j);
-            uchar temp = pixel[0];
-            pixel[0] = pixel[1];
-            pixel[1] = pixel[2];
-            pixel[2] = temp;
-            outputImg.at<Vec3b>(i, j) = pixel;
+    std::srand(std::time(0));
+    for (int i = 1; i < img.rows - 1; i++) {
+        for (int j = 1; j < img.cols - 1; j++) {
+
+            //distortion by selecting neighboring pixel at random
+            int r = std::rand() % 9;
+            Vec3b pixel = img.at<Vec3b>(i-1+r/3, j-1+r%3);
+            
+
+            //make it black white based on lowest channel value, with glare on top-left image corner
+            float alpha = float((img.rows-i+1)*(img.cols-j+1))/float((img.rows-1)*(img.cols-1));
+            uchar minVal = min(min(pixel[0], pixel[1]), pixel[2]);
+            uchar newVal = (1-alpha)*minVal + alpha*255;
+            
+            //assign new pixel values
+            outputImg.at<Vec3b>(i, j) = Vec3b(newVal, newVal, newVal);
+
         }
     }
     return outputImg;
