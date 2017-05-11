edbf7cef4fed935f4d07ebbbd0a223661c501185
michaelkihm <michael.kihm@web.de>
2017-05-11 15:26:39 +0200

    Add files via upload

---
+++ Aia1.cpp
@@ -0,0 +1,151 @@
+//============================================================================
+// Name        : Aia1.cpp
+// Author      : Ronny Haensch
+// Version     : 1.0
+// Copyright   : -
+// Description :
+//============================================================================
+
+#include "Aia1.h"
+
+// function that performs some kind of (simple) image processing
+/*
+ img	input image
+ return	output image
+ */
+Mat Aia1::doSomethingThatMyTutorIsGonnaLike(Mat& img){
+    Mat outputImg = Mat(img.size(), CV_8UC3); ;
+    int row,col;
+    //Compute the “negative” of a RGB image
+    for (row = 0; row < img.size().height; row++) {
+        for (col = 0; col < img.size().width; col++) {
+            cv::Vec3b sourceVector = img.at<cv::Vec3b>(row,col);
+            cv::Vec3b inverseVector = cv::Vec3b(255 - sourceVector[0], 255 - sourceVector[1], 255 - sourceVector[2]);
+            outputImg.at<cv::Vec3b>(row, col) = inverseVector;
+            
+            
+        }
+ 
+    return outputImg;
+    
+}
+/* *****************************
+ GIVEN FUNCTIONS
+ ***************************** */
+
+// function loads input image, calls processing function, and saves result
+/*
+ fname	path to input image
+ */
+void Aia1::run(string fname){
+    
+    // window names
+    string win1 = string ("Original image");
+    string win2 = string ("Result");
+    
+    // some images
+    Mat inputImage, outputImage;
+    
+    // load image
+    cout << "load image" << endl;
+    inputImage = imread( fname );
+    cout << "done" << endl;
+    
+    // check if image can be loaded
+    if (!inputImage.data){
+        cerr << "ERROR: Cannot read file " << fname << endl;
+        cout << "Press Enter to continue..." << endl;
+        cin.get();
+        exit(-1);
+    }
+    
+    // show input image
+    namedWindow( win1.c_str() );
+    imshow( win1.c_str(), inputImage );
+    
+    // do something (reasonable!)
+    outputImage = doSomethingThatMyTutorIsGonnaLike( inputImage );
+    
+    // show result
+    namedWindow( win2.c_str() );
+    imshow( win2.c_str(), outputImage );
+    
+    // save result
+    imwrite("result.jpg", outputImage);
+    
+    // wait a bit
+    waitKey(0);
+    
+}
+
+// function loads input image and calls processing function
+// output is tested on "correctness"
+/*
+ fname	path to input image
+ */
+void Aia1::test(string fname){
+    
+    // some image variables
+    Mat inputImage, outputImage;
+    
+    // load image
+    inputImage = imread( fname );
+    
+    // check if image can be loaded
+    if (!inputImage.data){
+        cerr << "ERROR: Cannot read file " << fname << endl;
+        exit(-1);
+    }
+    
+    // create output
+    outputImage = doSomethingThatMyTutorIsGonnaLike( inputImage );
+    // test output
+    test_doSomethingThatMyTutorIsGonnaLike(inputImage, outputImage);
+    
+}
+
+// function loads input image and calls processing function
+// output is tested on "correctness"
+/*
+ inputImage	input image as used by doSomethingThatMyTutorIsGonnaLike()
+ outputImage	output image as created by doSomethingThatMyTutorIsGonnaLike()
+ */
+void Aia1::test_doSomethingThatMyTutorIsGonnaLike(Mat& inputImage, Mat& outputImage){
+    
+    // ensure that input and output have equal number of channels
+    if ( (inputImage.channels() == 3) and (outputImage.channels() == 1) )
+        cvtColor(inputImage, inputImage, CV_BGR2GRAY);
+    
+    // split (multi-channel) image into planes
+    vector<Mat> inputPlanes, outputPlanes;
+    split( inputImage, inputPlanes );
+    split( outputImage, outputPlanes );
+    
+    // number of planes (1=grayscale, 3=color)
+    long numOfPlanes = inputPlanes.size();
+    
+    // calculate and compare image histograms for each plane
+    Mat inputHist, outputHist;
+    // number of bins
+    int histSize = 100;
+    float range[] = { 0, 256 } ;
+    const float* histRange = { range };
+    bool uniform = true; bool accumulate = false;
+    double sim = 0;
+    for(int p = 0; p < numOfPlanes; p++){
+        // calculate histogram
+        calcHist( &inputPlanes[p], 1, 0, Mat(), inputHist, 1, &histSize, &histRange, uniform, accumulate );
+        calcHist( &outputPlanes[p], 1, 0, Mat(), outputHist, 1, &histSize, &histRange, uniform, accumulate );
+        // normalize
+        inputHist = inputHist / sum(inputHist).val[0];
+        outputHist = outputHist / sum(outputHist).val[0];
+        // similarity as histogram intersection
+        sim += compareHist(inputHist, outputHist, CV_COMP_INTERSECT);
+    }
+    sim /= numOfPlanes;
+    
+    // check whether images are to similar after transformation
+    if (sim >= 0.8)
+        cout << "The input and output image seem to be quite similar (similarity = " << sim << " ). Are you sure your tutor is gonna like your work?" << endl;
+    
+}
---
+++ Aia1.h
@@ -0,0 +1,37 @@
+//============================================================================
+// Name        : Aia1.h
+// Author      : Ronny Haensch
+// Version     : 1.0
+// Copyright   : -
+// Description : header file for first AIA assignment
+//============================================================================
+
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+
+using namespace std;
+using namespace cv;
+
+class Aia1{
+
+	public:
+		// constructor
+		Aia1(void){};
+		// destructor
+		~Aia1(void){};
+		
+		// processing routine
+		void run(string);
+		// testing routine
+		void test(string);
+
+	private:
+		// function that performs some kind of (simple) image processing
+		// --> edit ONLY this function!
+		Mat doSomethingThatMyTutorIsGonnaLike(Mat&);
+
+		// test function
+		void test_doSomethingThatMyTutorIsGonnaLike(Mat&, Mat&);
+};
