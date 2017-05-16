d80b5e5832ce42d38f16691515af980da060df27
michaelkihm <michael.kihm@web.de>
2017-05-16 16:46:30 +0200

    Add files via upload

--- exercise2/Aia2.h
+++ exercise2/Aia2.h
@@ -7,7 +7,9 @@
 //============================================================================
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
 
 using namespace std;
 using namespace cv;
--- exercise2/main.cpp
+++ exercise2/main.cpp
@@ -15,33 +15,33 @@ using namespace std;
 // usage: path to image in argv[1]
 // main function. loads and saves image
 int main(int argc, char** argv) {
-
-	// will contain path to input image (taken from argv[1])
-	string img, tmpl1, tmpl2;
-
-	// check if image path was defined
-	// check if image paths were defined
-	if (argc != 4){
-	    cerr << "Usage: aia2 <input image>  <class 1 example>  <class 2 example>" << endl;
-	    cerr << "Press enter to continue..." << endl;
-	    cin.get();
-	    return -1;
-	}else{
-	    // if yes, assign it to variable fname
-	    img = argv[1];
-	    tmpl1 = argv[2];
-	    tmpl2 = argv[3];
-	}
-	
-	// construct processing object
-	Aia2 aia2;
-
-	// run some test routines
-	aia2.test();
-
-	// start processing
-	aia2.run(img, tmpl1, tmpl2);
-
-	return 0;
-
+    
+    // will contain path to input image (taken from argv[1])
+    string img, tmpl1, tmpl2;
+    
+    // check if image path was defined
+    // check if image paths were defined
+    if (argc != 4){
+        cerr << "Usage: aia2 <input image>  <class 1 example>  <class 2 example>" << endl;
+        cerr << "Press enter to continue..." << endl;
+        cin.get();
+        return -1;
+    }else{
+        // if yes, assign it to variable fname
+        img = argv[1];
+        tmpl1 = argv[2];
+        tmpl2 = argv[3];
+    }
+    
+    // construct processing object
+    Aia2 aia2;
+    
+    // run some test routines
+    aia2.test();
+    
+    // start processing
+    //aia2.run(img, tmpl1, tmpl2);
+    
+    return 0;
+    
 }
